96dce1f8e05280e2be22c22f3466dfedcc192320
danielsan <danielizquierdofernandez89@gmail.com>
2017-10-01 16:34:09 +0100

    markers showing when map is clicked

--- src/js/directives/googleMap.js
+++ src/js/directives/googleMap.js
@@ -20,12 +20,44 @@ function googleMap() {
         center: { lat: 51.52, lng: -0.082 },
         zoom: 10
       });
+      const placesService = new google.maps.places.PlacesService(map);
+      let markers = [];
 
+      function removeMarkers() {
+        markers.forEach(marker => marker.setMap(null));
+        markers = [];
+      }
+
+      map.addListener('click', (e) => {
+
+        removeMarkers();
+        
+        map.panTo(e.latLng); // Animation pan to location clicked
+
+        placesService.nearbySearch({
+          location: e.latLng,
+          radius: 2000,
+          openNow: true,
+          type: 'bar'
+        }, (results, status) => {
+          if(status !== 'OK') return false;
+
+          markers = results.map(result => {
+            return new google.maps.Marker({
+              position: result.geometry.location,
+              map: map,
+              animation: google.maps.Animation.DROP
+            });
+          });
+        });
+      });
       const latLng = { lat: location.lat, lng: location.lng};
       const marker = new google.maps.Marker({
         position: latLng,
         map: map
+
       });
+
 
       $scope.$watch('center', () => {
         if(!$scope.center) return false;
@@ -35,3 +67,51 @@ function googleMap() {
     }
   };
 }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const mapDiv = document.querySelector('.map');
+//   const input = document.querySelector('#place');
+//   const autocomplete = new google.maps.places.Autocomplete(input, { types: ['establishment'] });
+//   let markers = [];
+//
+//   autocomplete.addListener('place_changed', () => {
+//     console.log(autocomplete.getPlace());
+//   });
+//
+//   const map = new google.maps.Map(mapDiv, {
+//     center: { lat: 51.51, lng: -0.09 },
+//     zoom: 14
+//   });
+//
+//   const placesService = new google.maps.places.PlacesService(map);
+//
+//   // REMOVE MAP MARKERS
+//   function removeMarkers() {
+//     markers.forEach(marker => marker.setMap(null));
+//     markers = [];
+//   }
+//
+//   map.addListener('click', (e) => {
+//
+//     removeMarkers();
+//
+//     map.panTo(e.latLng); // Animation pan to location clicked
+//
+//     placesService.nearbySearch({
+//       location: e.latLng,
+//       radius: 2000,
+//       openNow: true,
+//       type: 'bar'
+//     }, (results, status) => {
+//       if(status !== 'OK') return false;
+//
+//       markers = results.map(result => {
+//         return new google.maps.Marker({
+//           position: result.geometry.location,
+//           map: map,
+//           animation: google.maps.Animation.DROP
+//         });
+//       });
+//     });
+//   });
+// });
