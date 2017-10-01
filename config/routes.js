77d82f0eb691461b860a0c55cec0cfa04789f356
WillTDs <w_tye@hotmail.com>
2017-10-01 15:26:24 +0100

    added secureRoute

--- config/routes.js
+++ config/routes.js
@@ -2,19 +2,24 @@ const router = require('express').Router();
 const auth = require('../controllers/auth');
 const plans = require('../controllers/plans');
 const items = require('../controllers/items');
+const secureRoute = require('../lib/secureRoute.js');
 
 router.route('/plans')
+  .all(secureRoute)
   .get(plans.index)
   .post(plans.create);
 
 router.route('/plans/:id')
+  .all(secureRoute)
   .get(plans.show)
   .delete(plans.delete);
 
 router.route('/plans/:id/items')
+  .all(secureRoute)
   .post(items.create);
 
 router.route('/plans/:id/items/:itemId')
+  .all(secureRoute)
   .delete(items.delete);
 
 router.route('/register')
