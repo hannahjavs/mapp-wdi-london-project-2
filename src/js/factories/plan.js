4c734bc2b515f0fcfb729dd65cc8b997c8a0086a
WillTDs <w_tye@hotmail.com>
2017-10-03 17:18:52 +0100

    temp commit of styles

---
+++ src/js/factories/plan.js
@@ -0,0 +1,10 @@
+angular
+  .module('itineraryApp')
+  .factory('Plan', Plan);
+
+Plan.$inject = ['$resource'];
+function Plan($resource) {
+  return new $resource('/api/plans/:id', { id: '@id' }, {
+    update: { method: 'PUT' }
+  });
+}
