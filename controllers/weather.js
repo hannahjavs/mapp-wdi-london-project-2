ba90d7974654618200df4f02914d1fb6c8462ef5
WillTDs <w_tye@hotmail.com>
2017-10-04 09:57:26 +0100

    fixed weather location

--- controllers/weather.js
+++ controllers/weather.js
@@ -3,7 +3,7 @@ const rp = require('request-promise');
 function weatherProxy(req, res) {
   // Create a new request promise
   rp({
-    url: `https://api.darksky.net/forecast/${process.env.DARK_SKIES_API_KEY}/42.3601,-71.0589?units=si`,
+    url: `https://api.darksky.net/forecast/${process.env.DARK_SKIES_API_KEY}/51.5057,-0.12608?units=si`,
     method: 'GET',
     json: true
   })
