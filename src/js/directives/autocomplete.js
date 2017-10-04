9c79488af5d0a1ae7420672f785d532e96554bdb
danielsan <danielizquierdofernandez89@gmail.com>
2017-10-04 17:24:44 +0100

    plans showing in profile

--- src/js/directives/autocomplete.js
+++ src/js/directives/autocomplete.js
@@ -12,11 +12,12 @@ function autocomplete(){
     require: 'ngModel',
     link(scope, element, attrs, ngModel){
       const input = document.querySelector('#place');
-      const autocomplete = new google.maps.places.Autocomplete(input,{ types: ['establishment'] });
+      const autocomplete = new google.maps.places.Autocomplete(input,{ types: ['geocode'] });
       autocomplete.addListener('place_changed', () => {
         scope.location = autocomplete.getPlace().geometry.location.toJSON();
-        console.log(autocomplete.getPlace());
         ngModel.$setViewValue(element.val());
+        // circle.setCenter({ lat: pos.coords.latitude, lng: pos.coords.longitude });
+        // circle.setRadius(scope.radius);
       });
     }
   };
