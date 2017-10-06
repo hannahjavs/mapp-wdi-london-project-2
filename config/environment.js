a56d269a10243f5a11612a2abd83235cc6eace80
hannahjavs <hannahsiujadavji@gmail.com>
2017-10-06 12:03:16 +0100

    changed email url

--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,6 @@ const port = process.env.PORT || 4000;
 const env = process.env.NODE_ENV || 'development';
 const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/itineraryApp';
 const secret = process.env.SECRET || 'shh';
-const url = env === 'development' ? 'http://localhost:7000' : 'add heroku url here';
+const url = env === 'development' ? 'http://localhost:7000' : 'https://mapp-ga@herokuapp.com';
 
 module.exports = { port, env, dbURI, secret, url };
