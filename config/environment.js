ce750df5ade7f29ab610e3c5fb87e9b6652abecc
hannahjavs <hannahsiujadavji@gmail.com>
2017-10-06 12:14:07 +0100

    Updating config vars

--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,6 @@ const port = process.env.PORT || 4000;
 const env = process.env.NODE_ENV || 'development';
 const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/itineraryApp';
 const secret = process.env.SECRET || 'shh';
-const url = env === 'development' ? 'http://localhost:7000' : 'https://mapp-ga@herokuapp.com';
+const url = process.env.MAPP_APP_URL || 'http://localhost:7000';
 
 module.exports = { port, env, dbURI, secret, url };
